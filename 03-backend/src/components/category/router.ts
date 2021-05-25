acfe408da88915d4a6b9f1892e511a5067389b06
Petar Milovanovic <petar97m@gmail.com>
2021-05-25 11:12:10 +0200

    Reorganize component routes

---
+++ 03-backend/src/components/category/router.ts
@@ -0,0 +1,14 @@
+import express = require("express");
+import CategoryService from './service';
+import CategoryController from './controller';
+
+export default class CategoryRouter {
+    public static setupRoutes(application: express.Application) {
+        const categoryService: CategoryService = new CategoryService();
+        const categoryController: CategoryController = new CategoryController(categoryService);
+
+        application.get("/category",     categoryController.getAll.bind(categoryController));
+        application.get("/category/:id", categoryController.getById.bind(categoryController));
+
+    }
+}
