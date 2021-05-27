b1ee48854ebd1f43fad147bdb2b9872bf7165779
Petar Milovanovic <petar97m@gmail.com>
2021-05-27 19:52:15 +0200

    Create a base service class with usable methods

--- 03-backend/src/components/category/service.ts
+++ 03-backend/src/components/category/service.ts
@@ -1,16 +1,10 @@
 import CategoryModel from "./model";
 import * as mysql2 from 'mysql2/promise';
-import { resolve } from "path";
 import IErrorResponse from '../../common/IErrorResponse.interface';
 import { IAddCategory } from "./dto/AddCategory";
+import BaseService from '../../services/BaseService';
 
-class CategoryService {
-    private db: mysql2.Connection;
-
-    constructor(db: mysql2.Connection) {
-        this.db = db;
-    }
-
+class CategoryService extends BaseService<CategoryModel>{
     protected async adaptModel(row: any): Promise<CategoryModel> {
         const item: CategoryModel = new CategoryModel();
 
@@ -21,56 +15,11 @@ class CategoryService {
     }
 
     public async getAll(): Promise<CategoryModel[]|IErrorResponse> {
-        return new Promise<CategoryModel[]|IErrorResponse>(async (resolve) => {
-            const sql: string = "SELECT * FROM category;";
-            this.db.execute(sql)
-                .then(async result => {
-                    const rows = result[0];
-                    const lista: CategoryModel[] = [];
-
-                    if (Array.isArray(rows)) {
-                        for (const row of rows) {
-                            lista.push(await this.adaptModel(row));
-                        }
-                    }
-                    
-                    resolve(lista);
-                })
-                .catch(error => {
-                    resolve({
-                        errorCode: error?.errno,
-                        errorMessage: error?.sqlMessage
-                    });
-                });
-        });
+        return await this.getAllFromTable("category");
     }
 
     public async getById(categoryId: number): Promise<CategoryModel|IErrorResponse|null> {
-        return new Promise<CategoryModel|IErrorResponse|null>(async (resolve) => {
-            const sql: string = "SELECT * FROM category WHERE category_id = ?;";
-            this.db.execute(sql, [categoryId])
-                .then(async result => {
-                    const rows = result[0];
-
-                    if (!Array.isArray(rows)) {
-                        resolve(null);
-                        return;
-                    }
-        
-                    if (rows.length === 0) {
-                        resolve(null);
-                        return;
-                    }
-        
-                    resolve(await this.adaptModel(rows[0]));
-                })
-                .catch(error => {
-                    resolve({
-                        errorCode: error?.errno,
-                        errorMessage: error?.sqlMessage
-                    });
-                });
-        });
+        return await this.getByIdFromTable("category", categoryId);
     }
 
     public async add(data: IAddCategory): Promise<CategoryModel|IErrorResponse> {
